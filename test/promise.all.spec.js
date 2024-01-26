42b2ae74f1218d6aa09b4ada290dc8cf925d67a4
Eyal Mrejen <eyalmrejen@gmail.com>
2024-01-26 13:09:46 +0200

    adding more tests

--- test/promise.all.spec.js
+++ test/promise.all.spec.js
@@ -1,29 +1,4 @@
 
-const CustomPromise = {
-    all: (promises) => {
-        return new Promise((resolve, reject) => {
-            const results = [];
-    
-            if (promises.length === 0) {
-                resolve(results);
-                return;
-            }
-    
-            let pending = promises.length;
-    
-            promises.forEach((promise, idx) => {
-                Promise.resolve(promise).then((value) => {
-                    results[idx] = value;
-                    pending--;
-                    if (pending === 0) {
-                        resolve(results);
-                    }
-                }, reject);
-            });
-        });
-    }
-}
-
 
 describe('Promise all test suite', () => {
     const task = time => {
