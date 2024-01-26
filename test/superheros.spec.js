311c6eb9cb57e9e23bd392cee9fee9d8d4bb75b6
Eyal Mrejen <eyalmrejen@gmail.com>
2024-01-26 12:16:21 +0200

    adding skip test

--- test/superheros.spec.js
+++ test/superheros.spec.js
@@ -3,7 +3,7 @@
  *  2. Sum the heros score from new york only
  */
 
-describe('Superheros score testing suite', () => {
+describe.skip('Superheros score testing suite', () => {
     it('Should show the correct score of the heros from new york', () => {
         expect(getSuperherosScoreFrom('New York')).toEqual(378);
     });
