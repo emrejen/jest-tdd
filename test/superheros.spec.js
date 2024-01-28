df4a4b53126837b30dfae5589cca2161767f4951
Eyal Mrejen <eyalmrejen@gmail.com>
2024-01-28 19:06:39 +0200

    fix url

--- test/superheros.spec.js
+++ test/superheros.spec.js
@@ -1,10 +1,10 @@
 /**
- *  1. Get data from 'https://my-json-server.typicode.Som/emrejen/generic-apis/superheros'
+ *  1. Get data from 'https://my-json-server.typicode.com/emrejen/generic-apis/superheros'
  *  2. Sum the heros score from new york only
  */
 
 describe.skip('Superheros score testing suite', () => {
-    it('should show the correct score of the heros from new york', () => {
-        expect(getSuperherosScoreFrom('New York')).toEqual(378);
-    });
+  it('should show the correct score of the heros from new york', () => {
+    expect(getSuperherosScoreFrom('New York')).toEqual(378);
+  });
 });
